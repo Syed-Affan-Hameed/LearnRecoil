fd2e4afa6b355e7929251331413d5040c135b9a8
Syed Affan Hameed <syedaffanhameed@gmail.com>
2024-11-09 00:06:20 +0530

    Learn Recoil Basics counter complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,70 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+
+ 
+  return <>
+  <Counter></Counter>
+  </>;
+ function Counter() {
   const [count, setCount] = useState(0);
 
-  return <></>;
-  function Counter() {
-    const [count, setCount] = useState(0);
+  return (
+    <div>
+      <CurrentCount count ={count}/>
+      <Increase setCount={setCount} />
+      <Decrease setCount={setCount} />
+    </div>
+  );
+}
+
+function CurrentCount({count})
+{
+  return(
+    <div>
+      {count}
+    </div>
+  )
+}
+
+  function Increase({setCount}) {
+
+    //simpler more intuitive approach
+    // function decreaseCount() {
+    //   setCount(count - 1);
+    // }
+
+    // return (
+    //   <div>
+    //     <button onClick={decreaseCount}>Decrease</button>
+    //   </div>
+    // );
 
     return (
       <div>
-        {count}
-        <Increase setCount={setCount} />
-        <Decrease setCount={setCount} />
+        <button onClick={() => setCount(count=>count + 1)}>Increase</button>
       </div>
     );
   }
-  function Increase() {
+  function Decrease({setCount}) {
+
+    //simpler more intuitive approach
+    // function decreaseCount() {
+    //   setCount(count - 1);
+    // }
+
+    // return (
+    //   <div>
+    //     <button onClick={decreaseCount}>Decrease</button>
+    //   </div>
+    // );
+
+
+    //pro code
     return (
       <div>
-        <button>Increase</button>
-      </div>
-    );
-  }
-  function Decrease() {
-    return (
-      <div>
-        <button>Decrease</button>
+        <button onClick={() => setCount(count=>count - 1)}>Decrease</button>
       </div>
     );
   }
